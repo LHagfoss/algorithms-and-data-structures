122217c637585bb4a546a54384d693da933645f2
LHagfoss <lucas@paral.no>
2026-03-24 13:56:30 +0100

    implement StaticArray with basic operations: push, pop, get, delete_at, contains, and is_empty

--- src/data_structures/array.rs
+++ src/data_structures/array.rs
@@ -1,4 +1,67 @@
-struct Node {
-    index: i32,
-    value: i32,
+use crate::algorithms::binary_search::{is_in_unsorted, SortingAlgorithm};
+
+/// Static Array, static size of 10, length `usize`
+pub struct StaticArray {
+    data: [i32; 10],
+    length: usize
+}
+
+impl StaticArray {
+    /// New instance of StaticArray
+    pub fn new() -> Self {
+        Self {
+            data: [0; 10],
+            length: 0,
+        }
+    }
+
+    pub fn push(&mut self, value: i32) -> Result<(), &'static str> {
+        if self.length >= self.data.len() {
+            return Err("Array is full");
+        }
+
+        self.data[self.length] = value;
+        self.length += 1;
+        Ok(())
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        if self.is_empty() {
+            return None;
+        }
+
+        self.length -= 1;
+        Some(self.data[self.length])
+    }
+
+    pub fn get(&self, index: usize) -> Option<i32> {
+        if index >= self.length {
+            return None
+        }
+
+        Some(self.data[index])
+    }
+
+    pub fn delete_at(&mut self, index: usize) -> Result<(), &'static str> {
+        if index >= self.length {
+            return Err("Index out of bounds");
+        }
+
+        for i in index..(self.length - 1) {
+            self.data[i] = self.data[i + 1];
+        }
+
+        self.length -= 1;
+        Ok(())
+    }
+
+    pub fn contains(&self, target: i32) -> bool {
+        let mut temp_slice = self.data[0..self.length].to_vec();
+
+        is_in_unsorted(&mut temp_slice, target, SortingAlgorithm::MergeSort)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.length == 0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use crate::algorithms::binary_search::{is_in_sorted, is_in_unsorted, SortingAlgorithm};
+use crate::data_structures::array::StaticArray;
 
 pub mod algorithms;
 pub mod data_structures;
 
 fn main() {
-    let sorted_array: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let mut unsorted_array: [i32; 10] = [5, 4, 2, 6, 10, 8, 4, 2, 5, 4];
+    let mut sorted_array = StaticArray::new();
 
-    println!("{}", is_in_sorted(&sorted_array, 4));
-    println!("{}", is_in_unsorted(&mut unsorted_array, 4, SortingAlgorithm::MergeSort));
+    for i in 0..10 {
+        println!("pushed {}", i + 1);
+        sorted_array.push(i + 1).expect("Error pushing");
+    }
+
+    println!("{}", sorted_array.is_empty());
+
+    if let Some(value) = sorted_array.pop() {
+        println!("popped {}", value);
+    } else {
+        eprintln!("Stack is empty");
+    }
+
+    if let Some(value) = sorted_array.get(1) {
+        println!("index[1] = {}", value)
+    } else {
+        eprintln!("Index out of bounds")
+    }
+
+    println!("{:?}", sorted_array.delete_at(0));
 }
